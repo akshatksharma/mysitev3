31db2d9edbdb8074c88668847ce2822eb5c1b3f3
Akshat Sharma <akshatksharma@wustl.edu>
2022-01-01 19:42:46 -0600

    Added transitions between pages

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -1,5 +1,25 @@
+<script context="module">
+	export const load = async ({ page }) => ({
+		props: {
+			key: page.path
+		}
+	});
+</script>
+
+<script lang="ts">
+	import type { Pages } from '../types';
+	import Nav from './Nav.svelte';
+	import Transition from './Transition.svelte';
+
+	export let key = undefined;
+	export let currentPage: Pages = undefined;
+</script>
+
 <div class="page">
-	<slot />
+	<Nav {currentPage} />
+	<Transition refresh={key}>
+		<slot />
+	</Transition>
 </div>
 
 <style>
@@ -8,6 +28,5 @@
 		max-width: 700px;
 		margin: 0 auto;
 		padding: 1rem 2rem;
-		/* border: 2px solid red; */
 	}
 </style>
---
+++ src/components/Transition.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { fly } from 'svelte/transition';
+	export let refresh = '';
+</script>
+
+{#key refresh}
+	<div in:fly={{ x: -2, duration: 400, delay: 400 }} out:fly={{ x: 2, duration: 400 }}>
+		<slot />
+	</div>
+{/key}
