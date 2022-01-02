26003d882f37cbf6538c71b50d7227096239cc74
Akshat Sharma <akshatksharma@wustl.edu>
2022-01-01 18:10:47 -0600

    Updated projects page

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -8,18 +8,35 @@
 <Page>
 	<Nav currentPage={Pages.Projects} />
 	<div class="body">
+		<div class="wavy" />
 		<div class="list">
 			<div class="projectBox">
-				<img src="" alt="" class="image" />
+				<!-- <img src="" alt="" class="image" /> -->
 				<div class="content">
-					<h2>MazeViz</h2>
-					<p>05 / 2020</p>
-					<p>
+					<div class="header">
+						<h2>MazeViz</h2>
+						<p>May 2020</p>
+					</div>
+					<p class="description">
 						Mazeviz is a project I made to become more comfortable with vanilla Javascript, Graph
 						search algorithms, and visualization. I enjoy learning with visuals, so I wanted to make
-						concepts in class come to life in an easy to use and informative site. As a TA for my
-						school's data structures/algorithms class, I used this site to make these algorithms
-						'click' for the students I teach as well.
+						concepts in class come to life in an easy to use and informative site.
+					</p>
+				</div>
+			</div>
+			<div class="projectBox">
+				<!-- <img src="" alt="" class="image" /> -->
+				<div class="content">
+					<div class="header">
+						<h2>Grounded: The Pandemic Archive</h2>
+						<p>June 2020</p>
+					</div>
+					<p class="description">
+						Grounded: The Pandemic Archive is a project that serves to take a snapshot of the
+						COVID-19 pandemic. Users are encouraged share how they have found new meaning in the
+						physical spaces and objects around them while in quanatine. Each submission consists of
+						an image and a story, which can be recorded or typed. Built in collaboration with Design
+						for America, I led the site's frontend work.
 					</p>
 				</div>
 			</div>
@@ -27,3 +44,32 @@
 	</div>
 	<Footer />
 </Page>
+
+<style>
+	.projectBox {
+		margin-bottom: 2rem;
+	}
+	.projectBox h2 {
+		font-weight: 700;
+		font-size: 1.25rem;
+	}
+
+	.projectBox .header {
+		margin-bottom: 0.5rem;
+	}
+
+	.projectBox .description {
+		margin-bottom: 0.5rem;
+	}
+
+	.wavy {
+		margin-top: 3rem;
+		margin-bottom: 3rem;
+		width: 100%;
+		height: 150px;
+		background: linear-gradient(270deg, #fbd9cf, #f9d3e1, #c4e4f0, #bdece1);
+		background-size: 800% 800%;
+
+		animation: AnimationName 30s ease infinite;
+	}
+</style>
